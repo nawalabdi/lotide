ede3f332d6475c632c2dc1ed0f969e86d3bc887b
nawalabdi <nawalabdi03@gmail.com>
2022-11-15 00:12:56 -0500

    added emojis

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual("Bootcamp", "Bootcamp"));
+console.log(assertEqual(1, 4));
