b3b7459c902b1975d191334b3b36cef58b8652bc
nawalabdi <nawalabdi03@gmail.com>
2022-11-18 14:39:53 -0500

    map.js

--- head.js
+++ head.js
@@ -6,17 +6,16 @@ function assertEqual(actual, expected) {
   }
 }
 
+function head (arr) {
+  return arr[0];
+}
+
 //TEST CODE
 (assertEqual("Lighthouse Labs", "Bootcamp"));
 (assertEqual(1, 1));
 (assertEqual("Bootcamp", "Bootcamp"));
 (assertEqual(1, 4));
 
-
-function head (arr) {
-  return arr[0];
-}
-
-
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
