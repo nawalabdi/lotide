26ed2003617da6a04e71b98cfea269e87a2faf28
nawalabdi <nawalabdi03@gmail.com>
2022-11-21 14:13:35 -0500

    refactored and added test files for head.js and assertEqual.js

---
+++ test/assertEqual.test.js
@@ -0,0 +1,8 @@
+const assertEqual= require('../assertEqual');
+
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(3, 3);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 6);
---
+++ test/head.test.js
@@ -0,0 +1,10 @@
+const assertEqual= require('../assertEqual');
+const head = require('../head');
+
+
+
+assertEqual(head([5,9,12]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,]), 5);
+assertEqual(head([]), undefined);
+
