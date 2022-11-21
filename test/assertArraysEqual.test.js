e5f0a454ee68eab5b5383d7ab60676868f573082
nawalabdi <nawalabdi03@gmail.com>
2022-11-21 16:05:13 -0500

    refactored assertArraysEqual

---
+++ test/assertArraysEqual.test.js
@@ -0,0 +1,10 @@
+const assertArraysEqual= require('../assertArraysEqual');
+
+
+
+
+
+
+//tests
+assertArraysEqual([5,5,5],[5,5,5]); //returns true
+assertArraysEqual([9,6,5],[3,5,2]); // returns false
